01d05df215f4342afadfcb3b9d624d2316e9e4ad
Simon Watson <simon.watson.sjw@gmail.com>
2025-01-08 20:05:30 +1100

    [refactor] consolidation of package and config stages.

--- custom-modules/custom-ide-config.el
+++ custom-modules/custom-ide-config.el
@@ -1,11 +1,38 @@
 ;;; custom-ide-config.el --- Provide IDE-like features -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023
+;; Copyright (C) 2025
 ;; SPDX-License-Identifier: MIT
 
-;; Author: System Crafters Community
+;; Author: Simon Watson
 
 ;;; Commentary:
+;; A selection of packages to provide functionality for the UI.
+
+;; Eglot has been built-in since Emacs 29.
+
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+;; use packages for formatting code without lsp/eglot.
+(use-package reformatter)
+
+;; use ruff for python formatting.
+(use-package ruff-format)
+
+;; editorconfig is a cross-editor/ide configuration tool to control
+;; indentation, spaces vs tabs, etc.
+(use-package editorconfig)
+;; a minor mode to always keep your code indented while editing blocks of code.
+(use-package aggressive-indent)
+;; Jump to the definition of a function. Works using oldskool
+;; rgrep type approaches. (No fancy tree-sitter here!)
+;; https://github.com/jacktasia/dumb-jump
+(use-package dumb-jump)
+;; Get some yasnippets installed.
+;; https://github.com/AndreaCrotti/yasnippet-snippets
+(use-package yasnippet-snippets)
 
 ;; Eglot configuration.
 
