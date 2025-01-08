af9a5a9a00a15c61e650c3ea9c8863249d991ef3
Simon Watson <simon.watson.sjw@gmail.com>
2025-01-08 21:00:10 +1100

    [refactor] put elisp helper packages in their own module.

---
+++ custom-modules/elisp-packages.el
@@ -0,0 +1,30 @@
+;;; elisp-packages.el --- packages to wrangle e-lisp -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025
+;; SPDX-License-Identifier: MIT
+
+;; Author: Simon Watson
+
+;;; Commentary:
+
+;; Packages to provide base libraries extending Emacs functionality.
+
+;;; Code:
+
+;; dash.el: A modern list API for Emacs. No 'cl required.
+(use-package dash)
+
+;; The long lost Emacs string manipulation library.
+(use-package s)
+
+;; (use-package pkg-info)
+
+(use-package jump)
+
+
+;; package to process and display alerts.
+;; https://github.com/spegoraro/org-alert
+(use-package alert)
+
+(provide 'elisp-packages)
+;;; elisp-packages.el ends here.
