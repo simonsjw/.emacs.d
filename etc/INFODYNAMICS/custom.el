130ac44eac331ffee0f5fdcbd58b19868774f3ea
Simon Watson <simon.watson.sjw@gmail.com>
2025-01-08 21:28:35 +1100

    [refactor] all customisation should be done in init files.
    [refactor] still using customisation api when possible.

--- etc/INFODYNAMICS/custom.el
+++ etc/INFODYNAMICS/custom.el
@@ -3,20 +3,45 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/var/INFODYNAMICS/bookmarks")
- '(edebug-trace t)
- '(org-agenda-files
-   '("/home/simon/Documents/org/agenda/evie.org" "/home/simon/Documents/org/agenda/gracie.org" "/home/simon/Documents/org/agenda/todos.org" "/home/simon/Documents/org/contacts/contacts.org" "/home/simon/Documents/org/agenda/recurrent.org"))
+ '(bmkp-last-as-first-bookmark-file
+   "/home/simon/.emacs.d/var/INFODYNAMICS/bmkp/current-bookmark.el")
+
+ '(package-selected-packages '(org-plus-contrib))
+ '(pretty-speedbar-blank-page '(""))
+ '(pretty-speedbar-book '(""))
+ '(pretty-speedbar-box-closed '(""))
+ '(pretty-speedbar-box-open '(""))
+ '(pretty-speedbar-folder '("" t))
+ '(pretty-speedbar-folder-open '("" t))
+ '(pretty-speedbar-font "Symbols Nerd Font Mono")
+ '(pretty-speedbar-icon-size 20)
+ '(pretty-speedbar-info '(""))
+ '(pretty-speedbar-mail '(""))
+ '(pretty-speedbar-page '(""))
+ '(pretty-speedbar-tag '(""))
+ '(pretty-speedbar-tags '(""))
  '(safe-local-variable-values
-   '((eval olivetti-set-width 160)
-     (ispell-buffer-session-localwords "frameset")
-     (eval olivetti-set-width 140)))
- '(sql-connection-alist
-   '(("datastore"
-      (sql-product 'postgres)
-      (sql-user "dc")
-      (sql-database "datastore")
-      (sql-server "localhost")))))
+   '((checkdoc-package-keywords-flag)
+     (org-blank-before-new-entry
+      (heading . auto)
+      (plain-list-item . auto))
+     (org-list-description-max-indent . 5)
+     (org-list-two-spaces-after-bullet-regexp)
+     (eval olivetti-set-width 160)))
+ '(speedbar-add-supported-extension
+   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle" ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css" ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock" "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md" ".markdown" ".org" ".txt" "README"))
+ '(speedbar-directory-button-trim-method 'trim)
+ '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
+ '(speedbar-indentation-width 3)
+ '(speedbar-show-unknown-files t)
+ '(speedbar-smart-directory-expand-flag t)
+ '(speedbar-update-flag t)
+ '(speedbar-use-images t)
+ '(speedbar-vc-do-check t)
+ '(sr-speedbar-auto-refresh t)
+ '(sr-speedbar-max-width 170)
+ '(sr-speedbar-right-side nil)
+ '(sr-speedbar-width 40 t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,17 +50,25 @@
  '(default ((t (:inherit nil :weight regular :height 100 :family "fira code"))))
  '(cell-blank-face ((t (:background "#1B152D" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-blank-odd-face ((t (:background "#61647A" :box (:line-width 1 :color "#D2D6CE")))))
- '(cell-comment-2-face ((t (:foreground "#859900" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-comment-2-face ((t (:foreground "#5A6800" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-comment-face ((t (:foreground "#169C05" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-cursor-face ((t (:background "#4F9C02" :foreground "#A6BE00" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-default-face ((t (:foreground "#D2D6CE" :box (:line-width 1 :color "#D2D6CE")))))
  '(fixed-pitch ((t (:inherit nil :weight regular :height 100 :family "source code pro"))))
  '(fixed-pitch-serif ((t (:inherit nil :weight regular :height 100 :family "courier new"))))
- '(mode-line ((t nil)))
- '(modus-themes-heading-0 ((t (:foreground "#D2D6CE" :weight regular :height 300 :family "Impact"))) t)
- '(modus-themes-heading-1 ((t (:foreground "#D2D6CE" :weight regular :height 200 :family "Impact"))) t)
- '(modus-themes-heading-2 ((t (:foreground "#D2D6CE" :weight Bold :height 180 :family "arial"))) t)
- '(modus-themes-heading-3 ((t (:foreground "#D2D6CE" :weight Bold :height 160 :family "arial"))) t)
- '(modus-themes-heading-4 ((t (:foreground "#D2D6CE" :weight regular :height 140 :family "arial"))) t)
+ '(modus-themes-heading-0 ((t (:foreground "#D2D6CE" :weight Bold :height 150 :family "Impact"))) t)
+ '(modus-themes-heading-1 ((t (:foreground "#D2D6CE" :weight Bold :height 140 :family "arial"))) t)
+ '(modus-themes-heading-2 ((t (:foreground "#D2D6CE" :weight Bold :height 130 :family "arial"))) t)
+ '(modus-themes-heading-3 ((t (:foreground "#D2D6CE" :weight Bold :height 120 :family "arial"))) t)
+ '(modus-themes-heading-4 ((t (:foreground "#D2D6CE" :weight regular :height 120 :family "arial"))) t)
  '(modus-themes-heading-5 ((t (:foreground "#D2D6CE" :weight regular :height 80 :family "arial"))) t)
+ '(speedbar-button-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-directory-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-file-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-highlight-face ((t (:inherit 'popup-menu-selection-face))))
+ '(speedbar-selected-face ((t (:foreground "gray98" :underline nil))))
+ '(speedbar-separator-face ((t (:inherit 'org-level-2 :forground "#D2D6CE" :background "#1B152D"))))
+ '(speedbar-tag-face ((t (:inherit 'font-lock-variable-name-face))))
  '(variable-pitch ((t (:inherit nil :weight regular :height 100 :family "times new roman")))))
+
+
