c0f6a0d00847e8d3a3c555bbc48a2dd47a79c491
Simon Watson <simon.watson.sjw@gmail.com>
2025-01-10 16:07:29 +1100

    [gitRefactor] added custom files setup to git.

---
+++ etc/SYSTEM76/custom.el
@@ -0,0 +1,54 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(icon-preference '(symbol image text emoji))
+ '(pretty-speedbar-blank-page '(""))
+ '(pretty-speedbar-book '(""))
+ '(pretty-speedbar-box-closed '(""))
+ '(pretty-speedbar-box-open '(""))
+ '(pretty-speedbar-folder '("" t))
+ '(pretty-speedbar-folder-open '("" t))
+ '(pretty-speedbar-font "Symbols Nerd Font Mono")
+ '(pretty-speedbar-icon-size 20)
+ '(pretty-speedbar-info '(""))
+ '(pretty-speedbar-mail '(""))
+ '(pretty-speedbar-page '(""))
+ '(pretty-speedbar-tag '(""))
+ '(pretty-speedbar-tags '(""))
+ '(projectile-speedbar-enable t)
+ '(safe-local-variable-values '((eval olivetti-set-width 160)))
+ '(semantic-sb-info-format-tag-function 'semantic-format-tag-short-doc)
+ '(speedbar-add-supported-extension
+   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle" ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css" ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock" "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md" ".markdown" ".org" ".txt" "README"))
+ '(speedbar-directory-button-trim-method 'trim)
+ '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
+ '(speedbar-indentation-width 3)
+ '(speedbar-show-unknown-files t)
+ '(speedbar-smart-directory-expand-flag t)
+ '(speedbar-update-flag t)
+ '(speedbar-use-images t)
+ '(speedbar-vc-do-check t)
+ '(sr-speedbar-auto-refresh t)
+ '(sr-speedbar-max-width 170)
+ '(sr-speedbar-right-side nil)
+ '(sr-speedbar-width 40 t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cell-blank-face ((t (:background "#1B152D" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-blank-odd-face ((t (:background "#4A4F6A" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-comment-2-face ((t (:foreground "#859B00" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-comment-face ((t (:foreground "#169C05" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-cursor-face ((t (:background "#4E9A06" :foreground "#CFD400" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-default-face ((t (:foreground "#D2D6CE" :box (:line-width 1 :color "#D2D6CE")))))
+ '(speedbar-button-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-directory-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-file-face ((t (:foreground "#D2D6CE"))))
+ '(speedbar-highlight-face ((t (:inherit 'popup-menu-selection-face))))
+ '(speedbar-selected-face ((t (:foreground "gray98" :underline nil))))
+ '(speedbar-separator-face ((t (:inherit 'org-level-2 :forground "#D2D6CE" :background "#1B152D"))))
+ '(speedbar-tag-face ((t (:inherit 'font-lock-variable-name-face)))))
