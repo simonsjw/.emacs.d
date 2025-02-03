615b05630159f8f1f916c4484136933bb2b65e4d
Simon Watson <simon.watson.sjw@gmail.com>
2025-02-03 20:33:59 +1100

    [debug] fixed up bookmark+

---
+++ var/INFODYNAMICS/bmkp/emacs-bmk-bmenu-state.el
@@ -0,0 +1,29 @@
+((last-sort-comparer
+  (bmkp-info-node-name-cp bmkp-url-cp bmkp-gnus-cp bmkp-local-file-type-cp
+                          bmkp-handler-cp)
+  bmkp-alpha-p)
+ (last-reverse-sort-p) (last-reverse-multi-sort-p)
+ (last-latest-bookmark-alist #1=
+                             (#("org" 0 3 (bmkp-full-record #1#))
+                              (buffer-name . "Dired: org")
+                              (front-context-string . "\n  drwxrwxr-x  2")
+                              (rear-context-string . " May 17 04:49 ..")
+                              (front-context-region-string)
+                              (rear-context-region-string) (visits . 0)
+                              (time . #2=(26208 22234 489963 36000))
+                              (created . #2#) (position . 126)
+                              (filename . "/home/simon/Documents/org/")
+                              (dired-directory . "~/Documents/org/")
+                              (dired-marked) (dired-switches . "-al")
+                              (dired-subdirs) (dired-hidden-dirs)
+                              (handler . bmkp-jump-dired)))
+ (last-bmenu-omitted-bookmarks) (last-bmenu-marked-bookmarks)
+ (last-bmenu-filter-function) (last-bmenu-filter-pattern . #3="")
+ (last-bmenu-title . "All Bookmarks") (last-bmenu-bookmark)
+ (last-specific-buffer . #3#) (last-specific-file . #3#)
+ (last-bmenu-toggle-filenames . t) (last-bmenu-before-hide-marked-alist)
+ (last-bmenu-before-hide-unmarked-alist)
+ (last-bookmark-file
+  . "/home/simon/.emacs.d/var/INFODYNAMICS/bmkp/current-bookmark.el")
+ (last-previous-bookmark-file
+  . "/home/simon/.emacs.d/var/INFODYNAMICS/bmkp/last-bookmark.el"))
