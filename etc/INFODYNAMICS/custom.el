d22536bf8a54c0e717d80d5bca031438f93e1ffc
Simon Watson <simon.watson.sjw@gmail.com>
2025-02-04 14:34:43 +1100

    [refactor] org-agenda and package.el settings added.

--- etc/INFODYNAMICS/custom.el
+++ etc/INFODYNAMICS/custom.el
@@ -6,13 +6,42 @@
  '(bmkp-last-as-first-bookmark-file
    "/home/simon/.emacs.d/var/INFODYNAMICS/bmkp/current-bookmark.el")
  '(icon-preference '(symbol image text emoji))
- '(org-agenda-files
-   '("/home/simon/Documents/org/agenda/todos.org" "/home/simon/Documents/org/agenda/evie.org" "/home/simon/Documents/org/agenda/gracie.org" "/home/simon/Documents/org/agenda/recurrent.org"))
  '(outline-minor-mode-cycle t)
  '(outline-minor-mode-highlight 'override)
  '(outline-minor-mode-prefix [3 64])
  '(outline-minor-mode-use-buttons 'in-margins)
- '(package-selected-packages '(org-plus-contrib))
+ '(package-selected-packages
+   '(aggressive-indent anaconda-mode apache-mode auctex-latexmk
+                       auto-compile bookmark+ cape cargo cargo-mode
+                       cargo-transient citar-embark clj-refactor
+                       consult-eglot-embark consult-project-extra
+                       corfu-terminal csv-mode dape dashboard dired+
+                       docker docker-compose-mode dumb-jump eldoc-box
+                       elisp-demos flycheck-clojure
+                       flymake-markdownlint flymake-shellcheck
+                       geiser-guile geiser-racket git-modes
+                       google-contacts helpful ibuffer-vc imenu-list
+                       info+ jump marginalia modus-themes
+                       nerd-icons-completion nerd-icons-corfu
+                       nerd-icons-dired nerd-icons-ibuffer
+                       no-littering numpydoc olivetti orderless
+                       org-alert org-appear org-contacts org-contrib
+                       org-link-beautify org-pretty-tags org-roam
+                       page-break-lines pandoc-mode pdf-tools pq
+                       pretty-speedbar pyvenv robots-txt-mode
+                       ruff-format rustic sly-asdf sly-quicklisp
+                       sly-repl-ansi-color sqlformat treesit-fold
+                       undo-tree vertico vterm web-mode
+                       yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((treesit-fold :url
+                   "https://github.com/emacs-tree-sitter/treesit-fold.git")
+     (dired+ :url "https://github.com/emacsmirror/dired-plus.git")
+     (info+ :url "https://github.com/emacsmirror/info-plus.git")
+     (bookmark+ :url
+                "https://github.com/emacsmirror/bookmark-plus.git")
+     (auto-compile :url
+                   "https://github.com/emacscollective/auto-compile.git")))
  '(pretty-speedbar-blank-page '(""))
  '(pretty-speedbar-book '(""))
  '(pretty-speedbar-box-closed '(""))
@@ -27,17 +56,14 @@
  '(pretty-speedbar-tag '(""))
  '(pretty-speedbar-tags '(""))
  '(projectile-speedbar-enable t)
- '(safe-local-variable-values
-   '((checkdoc-package-keywords-flag)
-     (org-blank-before-new-entry
-      (heading . auto)
-      (plain-list-item . auto))
-     (org-list-description-max-indent . 5)
-     (org-list-two-spaces-after-bullet-regexp)
-     (eval olivetti-set-width 160)))
+ '(safe-local-variable-values '((eval olivetti-set-width 160)))
  '(semantic-sb-info-format-tag-function 'semantic-format-tag-short-doc)
  '(speedbar-add-supported-extension
-   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle" ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css" ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock" "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md" ".markdown" ".org" ".txt" "README"))
+   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle"
+     ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css"
+     ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock"
+     "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md"
+     ".markdown" ".org" ".txt" "README"))
  '(speedbar-directory-button-trim-method 'trim)
  '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
  '(speedbar-indentation-width 3)
@@ -57,10 +83,10 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :weight regular :height 100 :family "fira code"))))
  '(cell-blank-face ((t (:background "#1B152D" :box (:line-width 1 :color "#D2D6CE")))))
- '(cell-blank-odd-face ((t (:background "#61647A" :box (:line-width 1 :color "#D2D6CE")))))
- '(cell-comment-2-face ((t (:foreground "#5A6800" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-blank-odd-face ((t (:background "#4A4F6A" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-comment-2-face ((t (:foreground "#859B00" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-comment-face ((t (:foreground "#169C05" :box (:line-width 1 :color "#D2D6CE")))))
- '(cell-cursor-face ((t (:background "#4F9C02" :foreground "#A6BE00" :box (:line-width 1 :color "#D2D6CE")))))
+ '(cell-cursor-face ((t (:background "#4E9A06" :foreground "#CFD400" :box (:line-width 1 :color "#D2D6CE")))))
  '(cell-default-face ((t (:foreground "#D2D6CE" :box (:line-width 1 :color "#D2D6CE")))))
  '(fixed-pitch ((t (:inherit nil :weight regular :height 100 :family "source code pro"))))
  '(fixed-pitch-serif ((t (:inherit nil :weight regular :height 100 :family "courier new"))))
