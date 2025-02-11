3b68ac10fff0a64e5a4cdc2007fd4d17e7c4a203
Simon Watson <simon.watson.sjw@gmail.com>
2025-02-11 11:18:02 +1100

    [Feature] integrated vc and nerd-fonts
    [feature] ibuffer-vc-set-filter-groups-by-vc-root and ibuffer-nerd-fonts were not playing nice. Created a local copy of ibuffer-vc to be better at playing nice with other ibuffer helpers.

--- custom-modules/ibuffer-support.el
+++ custom-modules/ibuffer-support.el
@@ -17,30 +17,155 @@
 
 (require 'ibuffer)
 (require 'ibuffer-vc)
+(require 'nerd-icons)
 
 ;;; Code:
 
 
 ;;;; Ibuffer column definitions
-;; Replace the `Size' column showing the size of the buffer with a human readable
-;; alternative.
-;; (see [[https://www.emacswiki.org/emacs/IbufferMode][IbufferMode]])
-(define-ibuffer-column size-h
-  (:name "Size"
-         :inline t
-         :summarizer
-         (lambda (column-strings)
-           (let ((total 0))
-             (dolist (string column-strings)
-               (setq total
-                     (+
-                      (float
-                       (my-strings/human-readable-file-sizes-to-bytes string))
-                      total)))
-             (my-strings/bytes-to-human-readable-file-sizes total)))              ; :summarizer nil
-         )
-  (my-strings/bytes-to-human-readable-file-sizes (buffer-size)))
 
+
+(defgroup nerd-icons-ibuffer nil
+  "Display nerd icons in ibuffer."
+  :group 'nerd-icons
+  :group 'ibuffer
+  :link '(url-link :tag "Homepage" "https://github.com/seagle0128/nerd-icons-ibuffer"))
+
+(defface nerd-icons-ibuffer-icon-face
+  '((t (:inherit default)))
+  "Face used for the icons while `nerd-icons-ibuffer-color-icon' is nil."
+  :group 'nerd-icons-ibuffer)
+
+(defface nerd-icons-ibuffer-dir-face
+  '((t (:inherit font-lock-doc-face)))
+  "Face used for the directory icon."
+  :group 'nerd-icons-ibuffer)
+
+(defface nerd-icons-ibuffer-size-face
+  '((t (:inherit font-lock-constant-face)))
+  "Face used for the size."
+  :group 'nerd-icons-ibuffer)
+
+(defface nerd-icons-ibuffer-mode-face
+  '((t (:inherit font-lock-keyword-face)))
+  "Face used for the major mode."
+  :group 'nerd-icons-ibuffer)
+
+(defface nerd-icons-ibuffer-file-face
+  '((t (:inherit completions-annotations)))
+  "Face used for the filename/process."
+  :group 'nerd-icons-ibuffer)
+
+(defcustom nerd-icons-ibuffer-icon t
+  "Whether display the icons."
+  :group 'nerd-icons-ibuffer
+  :type 'boolean)
+
+(defcustom nerd-icons-ibuffer-color-icon t
+  "Whether display the colorful icons.
+
+It respects `nerd-icons-color-icons'."
+  :group 'nerd-icons-ibuffer
+  :type 'boolean)
+
+(defcustom nerd-icons-ibuffer-icon-size 1.0
+  "The default icon size in ibuffer."
+  :group 'nerd-icons-ibuffer
+  :type 'float)
+
+(defcustom nerd-icons-ibuffer-human-readable-size t
+  "Use human readable file size in ibuffer."
+  :group 'nerd-icons-ibuffer
+  :type 'boolean)
+
+
+
+;; For alignment, the size of the name field should be the width of an icon
+
+;; create an icon for the entry from nerd-icons.
+(define-ibuffer-column icon
+  (:name "" :inline t)
+  (if nerd-icons-ibuffer-icon
+      (let ((icon (cond ((and (buffer-file-name) (nerd-icons-auto-mode-match?))
+                         (nerd-icons-icon-for-file
+                          (file-name-nondirectory (buffer-file-name))
+                          :height nerd-icons-ibuffer-icon-size))
+                        ((eq major-mode 'dired-mode)
+                         (nerd-icons-icon-for-dir
+                          (buffer-name)
+                          :height nerd-icons-ibuffer-icon-size
+                          :face 'nerd-icons-ibuffer-dir-face))
+                        (t
+                         (nerd-icons-icon-for-mode
+                          major-mode
+                          :height nerd-icons-ibuffer-icon-size)))))
+        (concat
+         (if (or (null icon) (symbolp icon))
+             (nerd-icons-faicon "nf-fa-file_o"
+                                :face (if nerd-icons-ibuffer-color-icon
+                                          'nerd-icons-dsilver
+                                        'nerd-icons-ibuffer-icon-face)
+                                :height nerd-icons-ibuffer-icon-size)
+           (if nerd-icons-ibuffer-color-icon
+               icon
+             (propertize icon
+                         'face
+                         `(:inherit nerd-icons-ibuffer-icon-face
+                                    :family ,(plist-get
+                                              (get-text-property 0 'face icon)
+                                              :family)))))
+         " "))
+    ""))
+
+
+(define-ibuffer-column mode+
+  (:name "Mode"
+         :inline t
+         :header-mouse-map ibuffer-mode-header-map
+         :props ('font-lock-face 'nerd-icons-ibuffer-mode-face
+                                 'mouse-face 'highlight
+	                         'keymap ibuffer-mode-name-map
+	                         'help-echo "mouse-2: filter by this mode"))
+  (format-mode-line mode-name nil nil (current-buffer)))
+
+
+(define-ibuffer-column filename-and-process+vc
+  (:name "Filename/Process"
+         :props ('font-lock-face 'nerd-icons-ibuffer-file-face)
+         :header-mouse-map ibuffer-filename/process-header-map
+         :summarizer
+         (lambda (strings)
+           (setq strings (delete "" strings))
+           (let ((procs 0)
+	         (files 0))
+             (dolist (string strings)
+               (when (get-text-property 1 'ibuffer-process string)
+                 (setq procs (1+ procs)))
+	       (setq files (1+ files)))
+             (concat (cond ((zerop files) "No files")
+		           ((= 1 files) "1 file")
+		           (t (format "%d files" files)))
+	             ", "
+	             (cond ((zerop procs) "no processes")
+		           ((= 1 procs) "1 process")
+		           (t (format "%d processes" procs)))))))
+  
+  (let ((proc (get-buffer-process buffer))
+	(filename (ibuffer-make-column-filename buffer mark)))
+    (if proc
+	(concat (propertize (format "(%s %s)" proc (process-status proc))
+			    'font-lock-face 'italic
+                            'ibuffer-process proc)
+		(if (> (length filename) 0)
+		    (format " %s" filename)
+		  ""))
+      ;; below logic used from ibuffer-vc
+      (if buffer-file-name
+          (let ((root (cdr (ibuffer-vc-root buffer))))
+            (if root
+                (file-relative-name buffer-file-name root)
+              (abbreviate-file-name buffer-file-name)))
+        filename))))
 
 ;; ;;;; Cursor wrapping
 ;; ;; wrap cursor movement at top and bottom of ibuffer.
@@ -102,6 +227,27 @@
 ;;   (interactive)
 ;;   (while (my-ibuffer/previous-line)))
 
+
+;; Replace the `Size' column showing the size of the buffer with a human 
+;; readable alternative.
+;; (see [[https://www.emacswiki.org/emacs/IbufferMode][IbufferMode]])
+(define-ibuffer-column size-h
+  (:name " Size"
+         :inline t
+         :summarizer
+         (lambda (column-strings)
+           (let ((total 0))
+             (dolist (string column-strings)
+               (setq total
+                     (+
+                      (float
+                       (my-strings/human-readable-file-sizes-to-bytes string))
+                      total)))
+             (my-strings/bytes-to-human-readable-file-sizes total)))              ; :summarizer nil
+         )
+  (my-strings/bytes-to-human-readable-file-sizes (buffer-size)))
+
+
 ;;; Customization:
 
 ;; prefer the more full-featured built-in ibuffer for managing
@@ -112,6 +258,12 @@
 ;; the number of hours before a buffer is considered "old" by
 ;; ibuffer.
 (customize-set-variable 'ibuffer-old-time 24)
+;; Use headerline
+(customize-set-variable 'ibuffer-use-header-line t)
+
+(custom-set-faces
+ '(ibuffer-header-face ((t (:foreground "cyan" :weight bold :underline t)))))
+
 
 ;; Modify the default ibuffer-formats. Note the two lists allow us to switch
 ;; between two views using the command ibuffer-switch-format.
@@ -119,32 +271,45 @@
       '(
         
         ;; Default view
-        (mark
-         modified
-         read-only
-         vc-status-mini
-         locked
+        ;; elide is the number of characters to limit the column at.
+        ((mark 1)
+         (icon 2)
+         (modified 1)
+         (vc-status-mini 1)
+         (read-only 1)
+         (locked 1)
          " "
-         (name 20 20 :left :elide)
+         (name 30 30 :left :elide)
          " "
-         (size-h 11 -1 :right)
+         (size-h 10 -1 :right)
+         "  "
+         (mode+ 16 16 :left :elide)
+         "  "
+         (vc-status 10 10 :left)
          " "
-         (mode 16 16 :left :elide)
-         " "
-         (vc-status 16 16 :left)
-         " "
-         vc-relative-file
-         " "
-         filename-and-process)
-        
+         (filename-and-process+vc 50 50 :left :elide)                             ; filename-and-process (incorporates logic from vc-relative-file.
+         )
         ;; Summary view
         (mark
          " "
          (name 16 -1)
          " "
          filename-and-process)
-        )
-      )
+        ))
+
+;; (setq-local header-line-format
+;;       "    MRVL Name                                 Size  Mode              VC_Status  Filename/Process")
+
+
+
+
+;; Here you may adjust by replacing :right with :center or :left
+;; According to taste, if you want the icon further from the name
+;; " " (icon 2 2)
+;; (name 18 18 :left :elide)
+;; " " (size-h 9 -1 :right)
+
+;; (name 16 -1)
 
 
 ;;; Keybindings:
@@ -156,11 +321,20 @@
 
 ;;; Hooks:
 
-;; (defun my-ibuffer/ibuffer-mode-config-hook ()
-;;   "Define a function to set the font in ibuffer."
-;;   (face-remap-add-relative 'default  :height 90))
-;; ;;  - then add it to a hook.
-;; (add-hook 'ibuffer-mode-hook 'my-ibuffer/ibuffer-mode-config-hook)
+(defun my-ibuffer/ibuffer-mode-config-hook ()
+  "Set up the ibuffer."
+  
+  ;; Define a function to set the font in ibuffer.
+  ;; (face-remap-add-relative 'default  :height 90)
+  
+  ;; sort ibuffer by vc status. 
+  ;;(ibuffer-do-sort-by-vc-status)
+
+  ;; apply filter groups by vc-root. 
+  (ibuffer-vc-set-filter-groups-by-vc-root)
+  )
+;;  - then add it to a hook.
+(add-hook 'ibuffer-mode-hook 'my-ibuffer/ibuffer-mode-config-hook)
 
 
 (provide 'ibuffer-support)
@@ -170,3 +344,4 @@
                                                                                   ; LocalWords:  ibuffer
                                                                                   ; LocalWords:  brust
                                                                                   ; LocalWords:  IbufferMode
+                                                                                  ; LocalWords:  defun
