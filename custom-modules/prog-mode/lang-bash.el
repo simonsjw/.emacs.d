6aa7f234a15acfd9def59a3f358a142b29176a42
Simon Watson <simon.watson.sjw@gmail.com>
2025-02-11 11:23:10 +1100

    [Feature] work on full bash-ts-mode support.

--- custom-modules/prog-mode/lang-bash.el
+++ custom-modules/prog-mode/lang-bash.el
@@ -1,60 +1,25 @@
-;;; lang-bash.el -*- lexical-binding: t; -*-
+;;; lang-bash.el --- Language support for Bash scripting -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 
-;; A config for editing Bash scripts with enhanced syntax highlighting
-;; via Treesit (bash-ts-mode), LSP code completion via Eglot, and
-;; on-the-fly syntax checking with Flymake.
+;; A config for editing Bash scripts with:
+;; - Enhanced syntax highlighting via bash-ts-mode (Tree-sitter)
+;; - LSP code completion via Eglot (if bash-language-server is available and Eglot configured)
+;; - On-the-fly syntax checking with Flymake (using shellcheck)
 ;;
 ;; Prerequisites:
-;;
-;; - Emacs with Tree-sitter installed and the Bash grammar for Tree-sit.
-;; - bash-language-server for LSP features.
-;; - shellcheck for Flymake syntax checking.
+;; - Emacs >= 29 with built-in Tree-sitter support
+;; - bash-language-server (npm i -g bash-language-server)
+;; - shellcheck
+
 
 ;;; Code:
 
-;;; Packages phase
+;; Always ensure flymake-shellcheck is loaded as a fallback
+(use-package flymake-shellcheck
+  :ensure t)
 
-(use-package flymake-shellcheck)
-
-(require 'sh-script)
-
-;; Assuming bash-ts-mode is set up to be used with Tree-sit
-;; and is either autoloaded or required elsewhere in your config.
-;;(require 'bash-ts-mode)
-
-;; Ensure we have 
-
-;; Eglot for LSP support
-;; (add-to-list 'eglot-server-programs
-;;             '(bash-ts-mode . ("bash-language-server" "start")))
-
-;; (defun my/bash-ts-mode-setup ()
-;;   "Setup function for `bash-ts-mode`."
-;;   (when (executable-find "bash-language-server")
-;;     (eglot-ensure)))
-
-;; (add-hook 'bash-ts-mode-hook 'my/bash-ts-mode-setup)
-
-;;(add-to-list 'auto-mode-alist '("\\.sh\\'" . bash-ts-mode))
-(add-hook 'shell-script-mode-hook
-          (lambda ()
-            ;; Activate bash-ts-mode, replace with the correct function to activate bash-ts-mode
-            (bash-ts-mode)
-
-            ;; Start flymake for shell scripts
-            (flymake-mode t)))
-
-;; (require 'eglot)
-
-;; Bash Language Server setup
-;; (add-hook 'bash-ts-mode-hook
-;;           (lambda ()
-;;             (eglot-ensure)))
-
-;; formatting function 
-(defun my/format-bash-script-with-shfmt ()
+(defun my-lang/format-bash-script-with-shfmt ()
   "Format the current Bash script using shfmt."
   (interactive)
   (let ((buffer (current-buffer))
@@ -63,80 +28,141 @@
     (goto-char point)))
 
 
-;; linting function. 
-;; (defun my/flymake-shellcheck-setup ()
-;;   "Configure Flymake to use shellcheck."
-;;   (when (and buffer-file-name
-;;              (string-match-p "\\.sh\\'" buffer-file-name))
-;;     (flymake-mode)
-;;     (flymake-shellcheck-load)))
-;; (add-hook 'sh-mode-hook 'my-flymake-shellcheck-setup)
+(defun my-lang/bash-mode-setup ()
+  "Setup function for `bash-ts-mode`."
+
+  ;; Use Eglot if bash-language-server is available
+  (if (executable-find "bash-language-server")
+      (eglot-ensure)
+    (progn
+      (flymake-mode)
+      (flymake-shellcheck-load)))
+
+;;;;; Set up outline
+
+  ;; Set up customisations for outline-minor-mode.
+  (setq-local outline-minor-mode-use-buttons 'in-margins)                         ; Show buttons
+  (setq-local outline-blank-line t)                                               ; Blank line before headers
+  (setq-local outline-minor-mode-highlight t)                                     ; Font-lock outlines
+  (setq-local outline-regexp "##+")                                               ; Match `##`
+  (setq-local outline-start "#")                                                  ; Start marker
+  (setq-local outline-level #'my-outline-mode/outline-level)                      ; Custom level function
+  (outline-minor-mode 1)                                                          ; Use outline-minor-mode
+
+;;;;; Folding
+
+  ;; Set the fringe mode for python-ts-mode folding.
+  (set-fringe-mode '(12 . 12))
+  
+  ;; Enable treesit-fold-mode
+  (treesit-fold-mode 1)
+  ;; Enable treesit-fold-indicators-mode
+  (treesit-fold-indicators-mode 1)
 
 
+  ;; You will probably want to tweak this variable, it determines how
+  ;; quickly the completion prompt provides LSP suggestions when
+  ;; typing. Be careful if you set it to 0 in a large project!
+  (customize-set-variable 'corfu-auto-delay 0.25)
 
-;; Flymake for syntax checking
-;; (require 'flymake-shellcheck)
-;; (add-hook 'bash-ts-mode-hook 'flymake-shellcheck-load)
+  ;; Enable Ya-snippets.
+  (yas-minor-mode 1)
 
-;; Keybindings and other functionalities from bash-mode and shell-mode
-;; may need to be manually integrated or adapted for bash-ts-mode.
-;; This could involve setting up specific keybindings or functions
-;; that replicate or call the equivalent functionality in bash-mode and shell-mode.
+;;;;; IDE layout
+  
+  ;; Provide a function to set the fill column indicator.
+  ;; This has a default of 80 but can be set on a per mode basis.
+  ;; Set the preferred fill column indicator for the mode and activate it.
+  
+  (setq display-fill-column-indicator-column 88)                                  ; comment indent
+  (setq fill-column 88)                                                           ; Column beyond which line wrapping occurs if it is activated.
+  (setq comment-fill-column 250)                                                  ; Column to use for wrapping comment lines.
+  (setq comment-column 90)                                                        ; Column to indent right-margin comments to.
 
-;; For example, to integrate a common shell-mode functionality:
-;; (with-eval-after-load 'bash-ts-mode
-;;   ;; Here you can define keybindings or functionalities specific to bash-ts-mode
-;;   ;; Example: (define-key bash-ts-mode-map (kbd "YOUR-KEYBINDING") 'SOME-FUNCTION)
+  (display-fill-column-indicator-mode 1)                                          ; show the buffer line width.
 
-
-;;   ;;;; IDE layout
-;;   ;;   ----------
-;;   ;; set preferred buffer width
-
-
-;;   ;;;; IDE functionality map
-;;   ;;   ---------------------
-;;   ;; compiling the code
-;;   ;; (keymap-set rust-ts-mode-map "C-c C-c C-u" #'rust-compile)
-
-;;   ;; checking the code
-;;   ;; (keymap-set rust-ts-mode-map "C-c C-c C-k" #'rust-check)
-
-;;   ;; testing
-;;   ;; (keymap-set rust-ts-mode-map "C-c C-c C-t" #'rust-test)
-
-;;   ;; running the code
-;;   ;; (keymap-set rust-ts-mode-map "C-c C-c C-r" #'rust-run)
-
-;; linting
-
-;;  formatting
-(keymap-set bash-ts-mode-map "C-c C-f" #'my/format-bash-script-with-shfmt)
-
-;;   ;; list all errors
-
-;;   ;; formatting errors
-;;   ;; (keymap-set rust-ts-mode-map "C-c C-n" #'rust-goto-format-problem)
-
-;;   ;; lsp-execute-code-action
-;;   ;; - run when lsp-ui displays code action at the top of the sideline
-
-;;   ;; xref-find-definitions
-
-;;   ;; xref-find-references
-
-;;   ;; rustic-cargo-add-missing-dependencies
-
-;;   ;;;; Project settings
-;;   ;;   ----------------
+  (dape-active-mode)                                                              ; ensure dape mode is active.
 
 
-;;   )
+;;;;; IDE functionality map
 
+  ;; checking the code
+  ;; (keymap-set bash-ts-mode-map "C-c C-c C-k" #'rust-check)
+
+  ;; testing
+  ;;  (keymap-set bash-ts-mode-map "C-c C-c C-t" #'rust-test)
+
+  ;; running the code
+  ;; (keymap-set bash-ts-mode-map "C-c C-c C-r" #'rust-run)
+
+  ;; linting
+
+  ;;  formatting
+  (keymap-set bash-ts-mode-map "C-c C-f" #'my-lang/format-bash-script-with-shfmt)
+
+  ;; list all errors
+
+  ;; formatting errors
+  ;; (keymap-set bash-ts-mode-map "C-c C-n" #'rust-goto-format-problem)
+
+  ;; lsp-execute-code-action
+  ;; - run when lsp-ui displays code action at the top of the sideline
+
+  ;; xref-find-definitions
+
+  ;; xref-find-references
+
+;;;;; Errors/linting
+  ;; list errors in buffer
+  (keymap-set bash-ts-mode-map "C-c e b" #'flymake-show-buffer-diagnostics)
+  ;; list errors in minibuffer
+  (keymap-set bash-ts-mode-map "C-c e m" #'consult-flymake)
+  ;; list errors in project
+  (keymap-set
+   bash-ts-mode-map "C-c e p" #'flymake-show-project-diagnostics)
+  ;; formatting errors (not applicable)
+  ;; (keymap-set bash-ts-mode-map "C-c C-n" )
+  ;; go to next error
+  (keymap-set bash-ts-mode-map "C-c e n" #'flymake-goto-next-error)
+  ;; go to previous error.
+  (keymap-set bash-ts-mode-map "C-c e l" #'flymake-goto-prev-error)
+
+;;;;; Variable/function references
+  ;; xref-find-definitions
+  ;; (keymap-set bash-ts-mode-map "M-." #'anaconda-mode-find-definitions)
+  ;; xref-find-references
+  ;;  (keymap-set bash-ts-mode-map "M-r" #'anaconda-mode-find-references)
+  ;; xref-find-assignments
+  ;;  (keymap-set bash-ts-mode-map "M-=" #'anaconda-mode-find-assignments)
+
+
+  (message
+   "[%s ; DEBUG; my-lang/bash-mode-setup]finished loading the defun ; ;"
+   (current-time-string)))
+
+(add-hook 'bash-ts-mode-hook 'my-lang/bash-mode-setup)
+
+
+;; set mode by file extension.
+(add-to-list 'auto-mode-alist '("\\.sh\\'" . bash-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc\\'" . bash-ts-mode))
+
+;; if a shebang is included in the file, choose mode by the implied interpreter,
+;; Note that this overrides any file extension map.
+(add-to-list 'interpreter-mode-alist '("bash" . bash-ts-mode))
+
+
+;; finally, ensure that any non-standard shebangs are covered. This overrides
+;; interpreter-mode-alist. The difference is that interpreter-mode-alist
+;; matches strictly the interpreter at the end of the shebang. Magic-mode-alist
+;; can match any regular expression against the first line in a file.
+(add-to-list 'magic-mode-alist
+             '((lambda ()
+                 (looking-at "^#!.*\\(bash\\|sh\\)")) . bash-ts-mode))
 
 (provide 'lang-bash)
 ;;; lang-bash.el ends here
 
 
 
-; LocalWords:  Treesit
+                                                                                  ; LocalWords:  Treesit
