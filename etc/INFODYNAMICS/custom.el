043ba4331a052a93402c86c7ff877b3338da6108
Simon Watson <simon.watson.sjw@gmail.com>
2025-02-11 11:04:53 +1100

    [Refactor] Org-agenda file paths added. Some text reformatting.

--- etc/INFODYNAMICS/custom.el
+++ etc/INFODYNAMICS/custom.el
@@ -6,42 +6,40 @@
  '(bmkp-last-as-first-bookmark-file
    "/home/simon/.emacs.d/var/INFODYNAMICS/bmkp/current-bookmark.el")
  '(icon-preference '(symbol image text emoji))
+ '(org-agenda-files
+   '("/home/simon/Documents/org/agenda/todos.org"
+     "/home/simon/Documents/org/agenda/gracie.org"
+     "/home/simon/Documents/org/agenda/evie.org"
+     "/home/simon/Documents/org/agenda/recurrent.org"))
  '(outline-minor-mode-cycle t)
  '(outline-minor-mode-highlight 'override)
  '(outline-minor-mode-prefix [3 64])
  '(outline-minor-mode-use-buttons 'in-margins)
  '(package-selected-packages
-   '(aggressive-indent anaconda-mode apache-mode auctex-latexmk
-                       auto-compile bookmark+ cape cargo cargo-mode
-                       cargo-transient citar-embark clj-refactor
-                       consult-eglot-embark consult-project-extra
-                       corfu-terminal csv-mode dape dashboard dired+
-                       docker docker-compose-mode dumb-jump eldoc-box
-                       elisp-demos flycheck-clojure
-                       flymake-markdownlint flymake-shellcheck
-                       geiser-guile geiser-racket git-modes
-                       google-contacts helpful ibuffer-vc imenu-list
-                       info+ jump marginalia modus-themes
-                       nerd-icons-completion nerd-icons-corfu
-                       nerd-icons-dired nerd-icons-ibuffer
-                       no-littering numpydoc olivetti orderless
-                       org-alert org-appear org-contacts org-contrib
-                       org-link-beautify org-pretty-tags org-roam
-                       page-break-lines pandoc-mode pdf-tools pq
-                       pretty-speedbar pyvenv robots-txt-mode
-                       ruff-format rustic sly-asdf sly-quicklisp
-                       sly-repl-ansi-color sqlformat treesit-fold
-                       undo-tree vertico vterm web-mode
+   '(aggressive-indent anaconda-mode apache-mode auctex-latexmk auto-compile
+                       bookmark+ cape cargo cargo-mode cargo-transient
+                       citar-embark clj-refactor compile-angel
+                       consult-eglot-embark consult-project-extra corfu-terminal
+                       csv-mode dape dashboard delight dired+ docker
+                       docker-compose-mode dumb-jump eldoc-box elisp-demos
+                       flycheck-clojure flymake-markdownlint flymake-shellcheck
+                       geiser-guile geiser-racket git-modes google-contacts
+                       helpful ibuffer-vc imenu-list info+ jump marginalia
+                       modus-themes nerd-icons-completion nerd-icons-corfu
+                       nerd-icons-dired nerd-icons-ibuffer no-littering numpydoc
+                       olivetti orderless org-alert org-appear org-contacts
+                       org-contrib org-link-beautify org-pretty-tags org-roam
+                       page-break-lines pandoc-mode pdf-tools pq pretty-speedbar
+                       pyvenv robots-txt-mode ruff-format rustic sly-asdf
+                       sly-quicklisp sly-repl-ansi-color sqlformat sr-speedbar
+                       treesit-fold undo-tree vertico vterm web-mode
                        yasnippet-snippets))
  '(package-vc-selected-packages
-   '((treesit-fold :url
-                   "https://github.com/emacs-tree-sitter/treesit-fold.git")
+   '((treesit-fold :url "https://github.com/emacs-tree-sitter/treesit-fold.git")
      (dired+ :url "https://github.com/emacsmirror/dired-plus.git")
      (info+ :url "https://github.com/emacsmirror/info-plus.git")
-     (bookmark+ :url
-                "https://github.com/emacsmirror/bookmark-plus.git")
-     (auto-compile :url
-                   "https://github.com/emacscollective/auto-compile.git")))
+     (bookmark+ :url "https://github.com/emacsmirror/bookmark-plus.git")
+     (auto-compile :url "https://github.com/emacscollective/auto-compile.git")))
  '(pretty-speedbar-blank-page '(""))
  '(pretty-speedbar-book '(""))
  '(pretty-speedbar-box-closed '(""))
@@ -59,11 +57,10 @@
  '(safe-local-variable-values '((eval olivetti-set-width 160)))
  '(semantic-sb-info-format-tag-function 'semantic-format-tag-short-doc)
  '(speedbar-add-supported-extension
-   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle"
-     ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css"
-     ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock"
-     "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md"
-     ".markdown" ".org" ".txt" "README"))
+   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle" ".properties"
+     ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css" ".less" ".scss" ".sass"
+     ".py" ".p" ".q" ".k" ".rs" ".lock" "makefile" "MAKEFILE" "Makefile" ".json"
+     ".yaml" ".toml" ".md" ".markdown" ".org" ".txt" "README"))
  '(speedbar-directory-button-trim-method 'trim)
  '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
  '(speedbar-indentation-width 3)
