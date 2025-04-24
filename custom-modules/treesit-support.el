81034769b895df883219d519508442732d93c304
Simon Watson <simon.watson.sjw@gmail.com>
2025-04-24 12:59:46 +1000

    [feature] save emacs windowtrees using this functionality.

--- custom-modules/treesit-support.el
+++ custom-modules/treesit-support.el
@@ -88,45 +88,48 @@ expects a form in the format of
 Only LANG and URL are mandatory.")
 
 (setq treesit-language-source-alist
- '((css
-     .
-     ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
-    (go
-     .
-     ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
-    (html
-     .
-     ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
-    (javascript
-     .
-     ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
-    (json
-     .
-     ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
-    (markdown
-     .
-     ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
-    (matlab
-     .
-     ("https://github.com/acristoffers/tree-sitter-matlab"))             ; added by simon watson
-    (python
-     .
-     ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
-    (rust
-     .
-     ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
-    (toml
-     .
-     ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
-    (tsx
-     .
-     ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
-    (typescript
-     .
-     ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-    (yaml
-     .
-     ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+      '((bash
+         .
+         ("https://github.com/tree-sitter/tree-sitter-bash" "v0.23.3"))
+        (css
+         .
+         ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+        (go
+         .
+         ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+        (html
+         .
+         ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+        (javascript
+         .
+         ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+        (json
+         .
+         ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+        (markdown
+         .
+         ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
+        (matlab
+         .
+         ("https://github.com/acristoffers/tree-sitter-matlab"))             ; added by simon watson
+        (python
+         .
+         ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+        (rust
+         .
+         ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+        (toml
+         .
+         ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+        (tsx
+         .
+         ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+        (typescript
+         .
+         ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+        (yaml
+         .
+         ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
 
 
 (defun my-treesitter/setup-install-grammars ()
