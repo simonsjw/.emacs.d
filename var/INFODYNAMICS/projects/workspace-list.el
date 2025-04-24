e11fe82ccf60d7324270fc5a190879ac3c1788d9
Simon Watson <simon.watson.sjw@gmail.com>
2025-04-24 13:11:15 +1000

    [tidy] list of project directories.

--- var/INFODYNAMICS/projects/workspace-list.el
+++ var/INFODYNAMICS/projects/workspace-list.el
@@ -1,6 +1,2 @@
 ;;; -*- lisp-data -*-
-(("/mnt/HDD04_WDD_08TB/workspace/")
- ("~/Downloads/github/")
- ("~/Documents/")
- ("~/Adventures/")
- ("~/dotfiles/"))
+(("/mnt/HDD04_WDD_08TB/workspace/") ("~/Downloads/github/") ("~/Documents/") ("~/Adventures/") ("~/dotfiles/") ("/home/simon/sync/primary/Documents/MATLAB/projects/SnP_detrended/"))
