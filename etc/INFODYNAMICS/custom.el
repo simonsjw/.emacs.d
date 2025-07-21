94f797e9aaa9353ab18472cc2883b059db067d97
Simon Watson <simon.watson.sjw@gmail.com>
2025-07-21 19:03:49 +1000

    [refactor] upstream changes led to change of this system file

--- etc/INFODYNAMICS/custom.el
+++ etc/INFODYNAMICS/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/var/INFODYNAMICS/bmkp/bookmark-default.bmk")
  '(bookmark-save-flag 1)
  '(icon-preference '(symbol image text emoji))
  '(outline-minor-mode-cycle t)
@@ -11,21 +12,26 @@
  '(outline-minor-mode-prefix [3 64])
  '(outline-minor-mode-use-buttons 'in-margins)
  '(package-selected-packages
-   '(aggressive-indent anaconda-mode apache-mode auctex-latexmk cape cargo
-                       cargo-mode cargo-transient citar-embark clj-refactor
-                       compile-angel consult-eglot-embark consult-project-extra
-                       corfu-terminal csv-mode dape dashboard delight dired+
-                       docker docker-compose-mode dumb-jump eldoc-box
-                       elisp-demos flycheck-clojure flymake-markdownlint
-                       flymake-shellcheck geiser-guile geiser-racket git-modes
-                       google-contacts helpful ibuffer-vc imenu-list info+ jump
-                       marginalia matlab-mode modus-themes nerd-icons-completion
-                       nerd-icons-corfu nerd-icons-dired no-littering numpydoc
-                       olivetti orderless org-alert org-appear org-contacts
-                       org-contrib org-link-beautify org-pretty-tags org-roam
-                       page-break-lines pandoc-mode pdf-tools pq pretty-speedbar
-                       pyvenv robots-txt-mode ruff-format rustic sly-asdf
-                       sly-quicklisp sly-repl-ansi-color sqlformat sr-speedbar
+   '(aggressive-indent anaconda-mode apache-mode auctex-latexmk cape
+                       cargo cargo-mode cargo-transient citar-embark
+                       clj-refactor compile-angel consult-eglot-embark
+                       consult-project-extra corfu-terminal csv-mode
+                       dape dashboard delight dired+ docker
+                       docker-compose-mode dot-env-mode dotenv-mode
+                       dumb-jump eldoc-box elisp-demos
+                       flycheck-clojure flymake-markdownlint
+                       flymake-shellcheck geiser-guile geiser-racket
+                       git-modes google-contacts helpful ibuffer-vc
+                       imenu-list info+ jump marginalia matlab-mode
+                       modus-themes nerd-icons-completion
+                       nerd-icons-corfu nerd-icons-dired no-littering
+                       numpydoc olivetti orderless org-alert
+                       org-appear org-contacts org-contrib
+                       org-link-beautify org-pretty-tags org-roam
+                       page-break-lines pandoc-mode pdf-tools pq
+                       pretty-speedbar pyvenv robots-txt-mode
+                       ruff-format rustic sly-asdf sly-quicklisp
+                       sly-repl-ansi-color sqlformat sr-speedbar
                        treesit-fold undo-tree vertico vterm web-mode
                        yasnippet-snippets))
  '(pretty-speedbar-blank-page '(""))
@@ -44,16 +50,17 @@
  '(safe-local-variable-values '((eval olivetti-set-width 160)))
  '(semantic-sb-info-format-tag-function 'semantic-format-tag-short-doc)
  '(speedbar-add-supported-extension
-   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle" ".properties"
-     ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css" ".less" ".scss" ".sass"
-     ".py" ".p" ".q" ".k" ".rs" ".lock" "makefile" "MAKEFILE" "Makefile" ".json"
-     ".yaml" ".toml" ".md" ".markdown" ".org" ".txt" "README"))
+   '(".cl" ".li?sp" ".lua" ".fnl" ".fennel" ".kt" ".mvn" ".gradle"
+     ".properties" ".cljs?" ".sh" ".bash" ".php" ".ts" ".html?" ".css"
+     ".less" ".scss" ".sass" ".py" ".p" ".q" ".k" ".rs" ".lock"
+     "makefile" "MAKEFILE" "Makefile" ".json" ".yaml" ".toml" ".md"
+     ".markdown" ".org" ".txt" "README"))
  '(speedbar-directory-button-trim-method 'trim)
  '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.\\.*$\\)\\'")
  '(speedbar-frame-parameters
-   '((name . "*SPEEDBAR*") (title . "*SPEEDBAR*") (minibuffer) (border-width . 4)
-     (width . 40) (menu-bar-lines . 0) (tool-bar-lines . 0) (unsplittable . t)
-     (left-fringe . 4)))
+   '((name . "*SPEEDBAR*") (title . "*SPEEDBAR*") (minibuffer)
+     (border-width . 4) (width . 40) (menu-bar-lines . 0)
+     (tool-bar-lines . 0) (unsplittable . t) (left-fringe . 4)))
  '(speedbar-indentation-width 3)
  '(speedbar-show-unknown-files t)
  '(speedbar-smart-directory-expand-flag t)
