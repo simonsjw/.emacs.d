19c802042550c58477147bd4a9325246b6d7c466
Simon Watson <simon.watson.sjw@gmail.com>
2025-12-29 15:16:23 +1100

    [gitRefactor] New projects created from emacs sub projects.
    [gitRefactor] these are added back to .emacs.d as git subprojects.

--- var/INFODYNAMICS/projects/workspace-list.el
+++ var/INFODYNAMICS/projects/workspace-list.el
@@ -1,2 +1,2 @@
 ;;; -*- lisp-data -*-
-(("/mnt/HDD04_WDD_08TB/workspace/") ("~/Downloads/github/") ("~/Documents/") ("~/Adventures/") ("~/dotfiles/") ("/home/simon/sync/primary/Documents/MATLAB/projects/SnP_detrended/"))
+(("/mnt/HDD04_WDD_08TB/workspace/") ("~/Downloads/github/") ("~/Documents/") ("~/Adventures/") ("~/dotfiles/"))
