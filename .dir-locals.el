7634c86fc1598fb81cabf194f71f395d273b04ad
Simon Watson <simon.watson.sjw@gmail.com>
2026-02-25 18:44:45 +1100

    [feature] project dict for jinx spellchecker

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "ARGS Args Backtrace DDirectory LaTeX LocalWords OPTARG README SPEEDBAR TODO Za alist aspell bar basedpyright cd conda config csv defconst defcustom defvar dir docstring docstrings dotfile el elisp elpa env flymake flyspell foo foobar github gitignore hdb http https hunspell idx ipynb ipython jdk joinpath json jsonl lvl md mnt modeline noqa odbc pdf prog py rlwrap scipy setq speedbar sql str sym tex tmp tstamp txt urls usr utf vterm ws yasnippet")
+         (jinx-project-spellings . t))))
